b22a9806401cf37ba4e97d22977bf4a5bf1500ef
Bartek Banachewicz <bananu7@o2.pl>
2015-12-19 23:13:53 +0100

    This now compiles with yet-nonexisting 0.1.4.1

--- src/Hate/Fonts.hs
+++ src/Hate/Fonts.hs
@@ -1,37 +1,37 @@
 module Hate.Fonts where
 
 import qualified Hate.Graphics as Hate
+import Hate.Math (Vec2(..))
 import qualified Data.Map as Map
 import Data.Maybe
 
-data CharacterRegion = CharacterRegion (Int, Int) (Int, Int)
+data CharacterRegion = CharacterRegion Vec2 Vec2
 
-data Font = Font (Map.Map Char CharacterRegion)
+type Font = Map.Map Char CharacterRegion
 
 -- | A pair of x-offset, region to sample
-type HateText = [(Int, CharacterRegion)]
+type HateText = [(Float, CharacterRegion)]
 
 toRegion :: Font -> Char -> Maybe CharacterRegion
-toRegion f c = lookup c f
+toRegion f c = Map.lookup c f
 
-toText :: String -> HateText
-toText str = zip offsets regions
+toText :: Font -> String -> HateText
+toText f str = zip offsets regions
     where
-        regions = catMaybes . map toRegion $ str
+        regions = catMaybes . map (toRegion f) $ str
 
         offsets = scanl calcOffset 0 regions
 
         -- we calculate width of each character and add it to the accumulator
-        calcOffset :: Int -> CharacterRegion -> Int
-        calcOffset off (CharacterRegion (x1, _) (x2, _)) = off + (x2 - x1)
+        calcOffset :: Float -> CharacterRegion -> Float
+        calcOffset off (CharacterRegion (Vec2 x1 _) (Vec2 x2 _)) = off + (x2 - x1)
 
 renderText :: Hate.Sprite -> HateText -> [Hate.DrawRequest]
 renderText s txt = map toDR txt
     where
-        toDR :: (Int, CharacterRegion) -> Hate.DrawRequest
-        toDR (off, (CharacterRegion (x1,y1) (x2,y2))) = 
-            Hate.translate (Vec2 (fromIntegral off, 0)) $
-            Hate.spritePart s ((x1, y1), (x2,y2))
+        toDR :: (Float, CharacterRegion) -> Hate.DrawRequest
+        toDR (off, (CharacterRegion a b)) = 
+            Hate.translate (Vec2 off 0) $ Hate.spritePart (a, b) s
 
-print :: Hate.Sprite -> String -> [Hate.DrawRequest]
-print spr = renderText spr . toText
+print :: Font -> Hate.Sprite -> String -> [Hate.DrawRequest]
+print f spr = renderText spr . toText f
